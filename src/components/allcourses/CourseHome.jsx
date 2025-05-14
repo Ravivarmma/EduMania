57b05184f106f33f62e97e97c036c2e969326e61
Ravivarmma <13ravivarmma13@gmail.com>
2025-05-14 10:47:40 +0800

    Fix case-sensitive import issue

--- src/components/allcourses/CourseHome.jsx
+++ src/components/allcourses/CourseHome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Back from "../common/back/Back";
-import CourseCard from "./CourseCard.jsx"; // Updated path assuming same folder
+import CourseCard from "../courses/courseCard";
 import OnlineCourses from "./OnlineCourses";
 
 const CourseHome = () => {
